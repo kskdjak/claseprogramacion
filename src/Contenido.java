b3fa27747273bb37e24f95b65d7a4e9587fd173d
Lenin Cuenca <loja_2002eceuador@hotmail.com>
2022-10-24 18:38:02 -0500

    Conceptos de la programacion

--- src/Contenido.java
+++ src/Contenido.java
@@ -1,6 +1,15 @@
 public class Contenido {
     public static void main(String[] args) {
-        System.out.println("CONCEPTOS DE LA PROGRAMACION");
-
+        System.out.println("CONCEPTOS DE LA PROGRAMACION\n");
+        System.out.println("1.-Tecnología:  La tecnología tiene el objetivo de eliminar las tareas repetitivas facilitando el trabajo\n y haciéndolo más eficiente.\n");
+        System.out.println("2.-Programacion: Se basa s el instrumento que permite la ejecución de las tareas automatizadas de un\n sistema informático.\n");
+        System.out.println("3.-Programa: Conjunto de instrucciones entendibles por el ordenador que permiten realizar un trabajo\n o resolver un problema.\n");
+        System.out.println("4.-Metodologia de la programacion: El conjunto de normas, métodos y anotaciones que nos indican la\n forma de programar Cada lenguaje de programación sigue una metodología distinta.\n");
+        System.out.println("5.-Lenguaje de programacion: Es un conjunto de reglas semánticas así como sintácticas que los\n programadores usan para la codificación de instrucciones de un programa o algoritmo de programación.\n");
+        System.out.println("6.-Entorno de desarrolo: Es el conjunto de herramientas utilizadas para la elaboración de un programa.\n");
+        System.out.println("7.-Recursos: Conjunto de componentes hardware que utilizaremos para la elaboración de un programa ejm.cpu, disco duro.\n");
+        System.out.println("8.-Sin solucocion: Esos problemas no pueden ser solucionados con los medios disponibles.\n");
+        System.out.println("9.-Determinados: Son los problemas tienen solución y además es única.\n");
+        System.out.println("10.-Inderterminados: Estos poseen un número indefinido de soluciones.\n");
     }
 }
