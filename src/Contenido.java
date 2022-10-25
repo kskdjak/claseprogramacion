c61c910e4db2f2e3dd2298340c2a155493974fd9
Lenin Cuenca <loja_2002eceuador@hotmail.com>
2022-10-24 19:09:51 -0500

    Conceptos de la programacion

--- src/Contenido.java
+++ src/Contenido.java
@@ -1,15 +1,25 @@
 public class Contenido {
     public static void main(String[] args) {
         System.out.println("CONCEPTOS DE LA PROGRAMACION\n");
-        System.out.println("1.-Tecnología:  La tecnología tiene el objetivo de eliminar las tareas repetitivas facilitando el trabajo\n y haciéndolo más eficiente.\n");
-        System.out.println("2.-Programacion: Se basa s el instrumento que permite la ejecución de las tareas automatizadas de un\n sistema informático.\n");
+        System.out.println("1.-Tecnologia:  La tecnologia tiene el objetivo de eliminar las tareas repetitivas facilitando el trabajo\n y haciéndolo más eficiente.\n");
+        System.out.println("2.-Programacion: Se basa s el instrumento que permite la ejecución de las tareas automatizadas de un\n sistema informatico.\n");
         System.out.println("3.-Programa: Conjunto de instrucciones entendibles por el ordenador que permiten realizar un trabajo\n o resolver un problema.\n");
-        System.out.println("4.-Metodologia de la programacion: El conjunto de normas, métodos y anotaciones que nos indican la\n forma de programar Cada lenguaje de programación sigue una metodología distinta.\n");
-        System.out.println("5.-Lenguaje de programacion: Es un conjunto de reglas semánticas así como sintácticas que los\n programadores usan para la codificación de instrucciones de un programa o algoritmo de programación.\n");
+        System.out.println("4.-Metodologia de la programacion: El conjunto de normas, metodos y anotaciones que nos indican la\n forma de programar Cada lenguaje de programación sigue una metodologia distinta.\n");
+        System.out.println("5.-Lenguaje de programacion: Es un conjunto de reglas semanticas así como sintacticas que los\n programadores usan para la codificacion de instrucciones de un programa o algoritmo de programación.\n");
         System.out.println("6.-Entorno de desarrolo: Es el conjunto de herramientas utilizadas para la elaboración de un programa.\n");
-        System.out.println("7.-Recursos: Conjunto de componentes hardware que utilizaremos para la elaboración de un programa ejm.cpu, disco duro.\n");
-        System.out.println("8.-Sin solucocion: Esos problemas no pueden ser solucionados con los medios disponibles.\n");
-        System.out.println("9.-Determinados: Son los problemas tienen solución y además es única.\n");
+        System.out.println("7.-Recursos: Conjunto de componentes hardware que utilizaremos para la elaboracion de un programa ejm.cpu, disco duro.\n");
+        System.out.println("8.-Sin solucion: Esos problemas no pueden ser solucionados con los medios disponibles.\n");
+        System.out.println("9.-Determinados: Son los problemas tienen solución y además es unica.\n");
         System.out.println("10.-Inderterminados: Estos poseen un número indefinido de soluciones.\n");
+        System.out.println("11.-Algoritmo: Es llevar a cabo una serie de pasos ordenados que tienen una secuencia la cual al hacerse\n una o mas veces se llega al mismo resultado.\n");
+        System.out.println("12.-Estructuras de control: Poder modificar el flujo de ejecución de un programa, obligando a este a\n realizar repetidamente una serie de acciones o seleccionar para su ejecucion una entre varias de ellas.\n");
+        System.out.println("13.-Código: Es el conjunto de palabras o símbolos que contienen instrucciones para la computadora. \n");
+        System.out.println("14.-Errores: Cuando se escribe un programa se pueden surgir errores.\n");
+        System.out.println("15.-Errores de compilación: Aparecen a la hora de compilar o interpretar el codigo fuente, estos errores son\n fáciles de corregir debido a que generalmente los traductores dan indicaciones del tipo y el lugar del error. \n");
+        System.out.println("16.-Errores de ejecución: Surgen al ejecutar el codigo ejecutable son difíciles de detectar y hacen que el\n programa termine de una forma no prevista ejm. division por cero.\n");
+        System.out.println("17.-Codigo Máquina: El codigo que la máquina puede entender y ejecutar.\n");
+        System.out.println("18.-Big Data: almacenamiento de grandes cantidades de datos.\n");
+        System.out.println("19.-Terminal: Es una interfaz de linea de comandos, es decir una interfaz es para que la persona\n envíe comandos a la computadora.\n");
+        System.out.println("20.-Codigo Binario: EL codigo formado solo por 0 y 1 que tiene como funcion codificar, es la base de toda la logica de la computadora.\n");
     }
 }
